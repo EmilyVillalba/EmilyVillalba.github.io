958d84e0385323ed70e83f396694c26f6d2c7d62
EmilyVillalba <emilycross05@gmail.com>
2025-02-28 15:12:52 -0600

    updated JavaScript for search- unsuccesful attempt

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,14 @@
 const amiiboContainer = document.querySelector(`.js-amiibo-container`);
 let form = document.querySelector(`.js-form`);
 let amiiboInput = document.querySelector(`[name=amiiboName]`);
+let amiibo = document.getElementById("search-amiibos").value;
 
 
 function loadAmiibos(amiiboName) {
   fetch(`https://amiiboapi.com/api/amiibo/`)
    .then(data => data.json())
    .then(response => {
-    const amiibo = response.amiibo;
+    let amiibo = response.amiibo;
 
     amiiboContainer.innerHTML = amiibo
         .map(amiibo => `
@@ -34,9 +35,30 @@ function loadAmiibos(amiiboName) {
     })
         .catch(err => {
             console.warn(err);
-            amiiboContainer.innerHTML = '<p class="error-message"><h2>Sorry we did not find an Amiibo with that name.</h2></p>';  
+            amiiboContainer.innerHTML = '<p class="error-message">Sorry we did not find an Amiibo with that name...</p>';  
     });
 }
+
+
+function searchAmiibos(amiiboName) {
+    let inputValue = document.getElementById("search-amiibos").value;
+    console.log(inputValue.value);
+    let amiibo = document.getElementById("amiibo-item").value;
+    
+    for (i = 0; i < amiibo.length; i++) {
+        let value = amiibo[i].getElementById("amiibo-item");
+
+        if (value) {
+            if (amiibo.length === 1) {
+                searchbar = amiiboContainer.innerHTML
+            }
+             else { 
+                return '<p class="error-message">Sorry we did not find an Amiibo with that name...</p>'; 
+            }
+        }
+    }
+}
+
 
 function formSubmitted(event) {
     event.preventDefault();
